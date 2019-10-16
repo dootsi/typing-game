7be9d5eac892025a9c81b3f2ca35ba40e3373862
a <alrie@protonmail.ch>
2019-10-16 21:43:56 +0200

    rewrite

---
+++ main.js
@@ -0,0 +1,106 @@
+const wordsElement = document.getElementById('words');
+const wordInput = document.getElementById('wordInput');
+const timerElement = document.getElementById('time');
+const wpmElement = document.getElementById('wpm');
+const restartButton = document.getElementById('restartButton');
+
+let randomWords = [];
+let currentIndex = 0;
+let started = false;
+let right = 0;
+let time = 0;
+let timer;
+
+const getWords = async () => {
+  const res = await fetch('words.json');
+  const data = await res.json();
+  return Promise.resolve(data);
+};
+
+const generateRandomWords = async () => {
+  const words = await getWords();
+  let generatedWords = [];
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < 10; i++) {
+    const randomIndex = Math.floor(Math.random() * (words.length - 0 + 1)) + 0;
+    const randomWord = words[randomIndex];
+    generatedWords = [...generatedWords, randomWord];
+  }
+  randomWords = generatedWords;
+};
+
+const displayWords = () => {
+  while (wordsElement.hasChildNodes()) {
+    wordsElement.removeChild(wordsElement.firstChild);
+  }
+  randomWords.forEach((word, i) => {
+    const span = document.createElement('span');
+    span.className = 'word';
+    span.textContent = word;
+    if (i === 0) {
+      span.className = 'word current';
+    }
+    wordsElement.appendChild(span);
+  });
+};
+
+const startTimer = () => {
+  if (started) {
+    timer = setTimeout(() => {
+      time += 1;
+      startTimer();
+    }, 1000);
+  } else {
+    timer = undefined;
+    started = false;
+  }
+};
+
+const checkWord = (e) => {
+  if (!started) {
+    started = true;
+    startTimer();
+  }
+  if (e.keyCode === 32) {
+    e.preventDefault();
+    const currentInputValue = wordInput.value;
+    wordInput.value = '';
+    if (started && currentIndex === randomWords.length - 1) {
+      started = false;
+      wpmElement.textContent = ((right / time) * 60).toFixed(2);
+      timerElement.textContent = time.toFixed(2);
+    }
+    const currentWord = randomWords[currentIndex];
+    const currentWordElement = wordsElement.childNodes[currentIndex];
+    const nextWordElement = wordsElement.childNodes[currentIndex + 1];
+    if (currentInputValue === currentWord) {
+      right += 1;
+      currentWordElement.className = 'word correct';
+    } else {
+      currentWordElement.className = 'word wrong';
+    }
+    nextWordElement.className = 'word current';
+    currentIndex += 1;
+  }
+};
+
+const restartGame = async () => {
+  wpmElement.textContent = '00';
+  timerElement.textContent = '00';
+  wordInput.value = '';
+  randomWords = [];
+  currentIndex = 0;
+  started = false;
+  right = 0;
+  time = 0;
+  timer = null;
+  await generateRandomWords();
+  displayWords();
+};
+
+(async () => {
+  await generateRandomWords();
+  displayWords();
+  wordInput.addEventListener('keypress', checkWord);
+  restartButton.addEventListener('click', restartGame);
+})();
